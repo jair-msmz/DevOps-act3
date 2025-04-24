5472d719e0e50ea426a7de569d0193ac917d56d8
jair-msmz <jairmsmz@outlook.com>
2025-04-24 15:22:42 -0600

    Darker Test 1

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     
-    return "¿Profe, ya se vio Dark?"
+    return render_template("index.html")
 
 if __name__=="__main__":
     port = int(os.environ.get("PORT", 5001))
