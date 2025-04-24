c27eaacb4b31508993a8e74440e2b8b873fe1bde
jair-msmz <jairmsmz@outlook.com>
2025-04-24 11:07:03 -0600

    Test CI 23

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     
-    return "¡Hola desde Flask en Docker! Estoy probando la versión 3"
+    return "¡Hola desde Flask en Docker! Estoy probando la versión 3.1"
 
 if __name__=="__main__":
     port = int(os.environ.get("PORT", 5001))
