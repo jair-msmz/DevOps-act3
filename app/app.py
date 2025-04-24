654b243361c9a787dc20fd1765c36e75229034d2
jair-msmz <jairmsmz@outlook.com>
2025-04-24 15:27:22 -0600

    Darker Test 2

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     
-    return render_template("index.html")
+    return render_template("templates/index.html")
 
 if __name__=="__main__":
     port = int(os.environ.get("PORT", 5001))
