773e5e6d51d51e0f200d97c673161c565d8f60fb
jair-msmz <jairmsmz@outlook.com>
2025-04-23 22:33:26 -0600

    Test CI 17

--- app/app.py
+++ app/app.py
@@ -8,4 +8,5 @@ def home():
     return "¡Hola desde Flask en Docker!"
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0", port=5001)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host="0.0.0.0", port=port)
