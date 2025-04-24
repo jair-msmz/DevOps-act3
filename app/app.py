70b2ecc7f63bd355c5bf377c0df0451c37884b74
jair-msmz <jairmsmz@outlook.com>
2025-04-24 15:30:48 -0600

    Darkest Timeline

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask
 import os
 
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     
-    return render_template("templates/index.html")
+    return "¿Profe, ya se vio Dark?"
 
 if __name__=="__main__":
     port = int(os.environ.get("PORT", 5001))
